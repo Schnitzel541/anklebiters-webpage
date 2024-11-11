ef85dfeede90affe75557e792d3ef4af2630697e
Schnitzel541 <j.budding122@gmail.com>
2024-11-11 22:46:30 +0100

    Add next/link

--- anklebiters-webpage/src/app/components/Header.tsx
+++ anklebiters-webpage/src/app/components/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header>
                 <ul className="header-ul" style={{margin: '0'}}>
                     <img className="header-logo" src="/hondje_blauw.png" alt="Band Logo" style={{maxWidth: '40px', padding: '10px'}}/>
-                    <li className="header-list-item" key="home"><a href="/">Home</a></li>
-                    <li className="header-list-item" key="over"><a href="/over">Over</a></li>
-                    <li className="header-list-item" key="merch"><a href="/merch">Merch</a></li>
-                    <li className="header-list-item" key="shows"><a href="/shows">Shows</a></li>
+                    <li className="header-list-item" key="home"><Link href="/">Home</Link></li>
+                    <li className="header-list-item" key="over"><Link href="/over">Over</Link></li>
+                    <li className="header-list-item" key="merch"><Link href="/merch">Merch</Link></li>
+                    <li className="header-list-item" key="shows"><Link href="/shows">Shows</Link></li>
                 </ul>
         </header>
     );
